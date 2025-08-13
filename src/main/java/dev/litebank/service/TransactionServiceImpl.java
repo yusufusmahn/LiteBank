1c36d5472847dd8fcd7e5abe07c749dc18b1aebe
yusufusmahn <yusuf.usman1003@gmail.com>
2025-08-13 16:50:16 +0100

    update

--- src/main/java/dev/litebank/service/TransactionServiceImpl.java
+++ src/main/java/dev/litebank/service/TransactionServiceImpl.java
@@ -6,6 +6,7 @@ import dev.litebank.dto.responses.TransactionResponse;
 import dev.litebank.model.Transaction;
 import dev.litebank.repository.TransactionRepository;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,28 +14,43 @@ import org.springframework.stereotype.Service;
 public class TransactionServiceImpl implements TransactionService{
 
     private final TransactionRepository transactionRepository;
+    private final ModelMapper modelMapper;
 
 
     @Override
     public CreateTransactionResponse create(CreateTransactionRequest transactionRequest) {
-        Transaction transaction = new Transaction();
-        transaction.setAmount(transactionRequest.getAmount());
-        transaction.setTransactionType(transactionRequest.getTransactionType());
-        transaction.setAccountNumber(transactionRequest.getAccountNumber());
+        Transaction transaction = createTransactionFrom(transactionRequest);
         transaction = transactionRepository.save(transaction);
+        return buildTransactionResponseFrom(transaction);
+    }
 
-        CreateTransactionResponse createTransactionResponse = new CreateTransactionResponse();
-        createTransactionResponse.setId(transaction.getId());
-        createTransactionResponse.setAmount(transactionRequest.getAmount().toString());
-        createTransactionResponse.setTransactionType(transactionRequest.getTransactionType().toString());
+    private CreateTransactionResponse buildTransactionResponseFrom(Transaction transaction) {
+//        CreateTransactionResponse createTransactionResponse = new CreateTransactionResponse();
+//        createTransactionResponse.setId(transaction.getId());
+//        createTransactionResponse.setAmount(transaction.getAmount().toString());
+//        createTransactionResponse.setTransactionType(transaction.getTransactionType().toString());
+//        return createTransactionResponse;
 
-        return createTransactionResponse;
+
+        return modelMapper.map(transaction, CreateTransactionResponse.class);
+
+    }
+
+    private Transaction createTransactionFrom(CreateTransactionRequest transactionRequest) {
+//        Transaction transaction = new Transaction();
+//        transaction.setAmount(transactionRequest.getAmount());
+//        transaction.setTransactionType(transactionRequest.getTransactionType());
+//        transaction.setAccountNumber(transactionRequest.getAccountNumber());
+//        return transaction;
+
+
+        return modelMapper.map(transactionRequest, Transaction.class);
+
     }
 
     @Override
     public TransactionResponse getTransactionById(String id) {
-        Transaction transaction = transactionRepository.findById(id).orElseThrow(()-> new RuntimeException("Transaction not found"));
-
+        Transaction transaction = transactionRepository.findById(id).orElseThrow(()-> new RuntimeException("transaction not found"));
         TransactionResponse transactionResponse = new TransactionResponse();
         transactionResponse.setAmount(transaction.getAmount().toString());
         return transactionResponse;
