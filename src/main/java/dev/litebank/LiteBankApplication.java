54aa4f0b3e39b5505f8320038d53eea21715fba9
yusufusmahn <yusuf.usman1003@gmail.com>
2025-08-13 16:47:52 +0100

    task added

--- src/main/java/dev/litebank/LiteBankApplication.java
+++ src/main/java/dev/litebank/LiteBankApplication.java
@@ -1,7 +1,9 @@
 package dev.litebank;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LiteBankApplication {
@@ -10,5 +12,9 @@ public class LiteBankApplication {
 		SpringApplication.run(LiteBankApplication.class, args);
 	}
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
 }
