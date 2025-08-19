c7f99c6b7f34e7c047adeb8df71f865167f4e69a
yusuf usman <yusuf.usman1003@gmail.com>
2025-08-19 11:01:48 +0100

    update

--- src/main/java/dev/litebank/repository/TransactionRepository.java
+++ src/main/java/dev/litebank/repository/TransactionRepository.java
@@ -2,10 +2,13 @@ package dev.litebank.repository;
 
 import dev.litebank.model.Transaction;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
 
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
-    Page<Transaction> getByAccountNumber(String accountNumber, Pageable pageable);
+//    Page<Transaction> getByAccountNumber(String accountNumber, Pageable pageable);
+
+    Page<Transaction> readByAccountNumber(String accountNumber, Pageable pageable);
+
 }
