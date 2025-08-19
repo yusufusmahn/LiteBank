ac85cf49e26b09cff68e6470b7bd36af08586dc5
yusuf usman <yusuf.usman1003@gmail.com>
2025-08-19 11:28:18 +0100

    update

--- src/test/java/dev/litebank/repository/TransactionRepositoryTest.java
+++ src/test/java/dev/litebank/repository/TransactionRepositoryTest.java
@@ -24,7 +24,7 @@ class TransactionRepositoryTest {
     void retrieveTransactionByAccountNumber() {
         String accountNumber = "0123456789";
         Pageable pageable = PageRequest.of(0, 5);
-        Page<Transaction>  transactions = transactionRepository.readByAccountNumber(accountNumber,pageable);
+        Page<Transaction>  transactions = transactionRepository.getByAccountNumber(accountNumber,pageable);
         assertThat(transactions).isNotNull();
         assertThat(transactions.getContent().size()).isEqualTo(5);
     }
