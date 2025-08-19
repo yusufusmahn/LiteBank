71b11d3ca7a2d60bcdccc77f02b25fd13834efd2
yusuf usman <yusuf.usman1003@gmail.com>
2025-08-19 12:03:34 +0100

    update

--- src/test/java/dev/litebank/service/TransactionServiceTest.java
+++ src/test/java/dev/litebank/service/TransactionServiceTest.java
@@ -5,13 +5,14 @@ import dev.litebank.dto.TransactionType;
 import dev.litebank.dto.requests.CreateTransactionRequest;
 import dev.litebank.dto.responses.CreateTransactionResponse;
 import dev.litebank.dto.responses.TransactionResponse;
-import dev.litebank.dto.responses.ViewAccountResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,7 +28,7 @@ public class TransactionServiceTest {
     private AccountService accountService;
 
     @Test
-    void testTransactionService(){
+    void testCreateTransactionService(){
         CreateTransactionRequest transactionRequest = new CreateTransactionRequest();
         transactionRequest.setTransactionType(TransactionType.CREDIT);
         transactionRequest.setAmount(new BigDecimal("20000.00"));
@@ -41,6 +42,16 @@ public class TransactionServiceTest {
         assertThat(transaction).isNotNull();
         assertThat(transaction.getAmount()).isEqualTo(transactionRequest.getAmount().toString());
 
+    }
+
+
+    @Test
+    @Sql(scripts = {"/db/data.sql"})
+    void testCanGetTransactionByAccountNumber(){
+        List<TransactionResponse> transactions =
+                transactionService.getTransactionsFor("0123456789",1,5);
+        assertThat(transactions).isNotNull();
+        assertThat(transactions.size()).isEqualTo(5);
     }
 
 //    @Test
