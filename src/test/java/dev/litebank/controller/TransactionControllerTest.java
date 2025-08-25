b8c3999a67188b396bac60f89f2a6efe9fcfcdfb
yusuf usman <yusuf.usman1003@gmail.com>
2025-08-25 16:59:59 +0100

    update

--- src/test/java/dev/litebank/controller/TransactionControllerTest.java
+++ src/test/java/dev/litebank/controller/TransactionControllerTest.java
@@ -1,7 +1,5 @@
 package dev.litebank.controller;
 
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
