fb0569a6004994c6810ad6bf28f850767de5099e
yusuf usman <yusuf.usman1003@gmail.com>
2025-08-25 16:53:41 +0100

    update

--- src/test/java/dev/litebank/service/NotificationServiceTest.java
+++ src/test/java/dev/litebank/service/NotificationServiceTest.java
@@ -1,9 +1,15 @@
 package dev.litebank.service;
 
 
+import dev.litebank.dto.requests.EmailNotificationRequest;
+import dev.litebank.dto.responses.EmailNotificationResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 public class NotificationServiceTest {
@@ -13,9 +19,15 @@ public class NotificationServiceTest {
 
 
     @Test
-    public void testSendEmailNotification() {
+    public void testSendEmailNotification() throws IOException {
+        EmailNotificationRequest notificationRequest = new EmailNotificationRequest();
+        notificationRequest.setRecipient("becaw65821@chaublog.com");
+        notificationRequest.setSubject("Test Subject");
+        notificationRequest.setMailBody("Test Body");
+        EmailNotificationResponse response =  notificationService.notifyBy(notificationRequest);
+        assertThat(response).isNotNull();
+        assertThat(response.isStatus()).isTrue();
 
     }
-
 
 }
