2692152fc1c069c948e6f309045c8fb906b47b22
yusuf usman <yusuf.usman1003@gmail.com>
2025-08-25 13:40:31 +0100

    update

--- src/main/java/dev/litebank/controller/TransactionController.java
+++ src/main/java/dev/litebank/controller/TransactionController.java
@@ -8,10 +8,7 @@ import dev.litebank.service.TransactionService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,8 +19,8 @@ public class TransactionController {
 
     private final TransactionService transactionService;
 
-    @GetMapping
-    public ResponseEntity<?> TransactionsByAccountNumber( String accountNumber){
+    @GetMapping("/{account_number}")
+    public ResponseEntity<?> TransactionsByAccountNumber(@PathVariable("account_number") String accountNumber){
         try{
             List<TransactionResponse> transactionResponses = transactionService
                     .getTransactionsFor(accountNumber);
@@ -33,5 +30,6 @@ public class TransactionController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(new ErrorResponse<>(e.getMessage()));
         }
+
     }
 }
