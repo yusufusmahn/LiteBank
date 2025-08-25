0dc1937d939ab90fe5a3d6adfd399db2e9c81601
yusuf usman <yusuf.usman1003@gmail.com>
2025-08-25 13:41:49 +0100

    update

---
+++ src/test/java/dev/litebank/service/NotificationServiceTest.java
@@ -0,0 +1,21 @@
+package dev.litebank.service;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NotificationServiceTest {
+
+    @Autowired
+    private NotificationService notificationService;
+
+
+    @Test
+    public void testSendEmailNotification() {
+
+    }
+
+
+}
