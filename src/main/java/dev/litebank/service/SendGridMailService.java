5c15bde4985f68a5b7f7a74e511a169c3612ac5d
yusuf usman <yusuf.usman1003@gmail.com>
2025-08-27 10:53:31 +0100

    update

---
+++ src/main/java/dev/litebank/service/SendGridMailService.java
@@ -0,0 +1,50 @@
+package dev.litebank.service;
+
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import dev.litebank.dto.requests.EmailNotificationRequest;
+import dev.litebank.dto.responses.EmailNotificationResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+
+@Service
+@Slf4j
+public class SendGridMailService implements EmailNotificationService{
+
+    @Value("${apiKey}")
+    private String apiKey;
+
+    @Override
+    public EmailNotificationResponse notifyBy(EmailNotificationRequest notificationRequest) throws IOException {
+
+        Email from = new Email("yusuf.usman3001@gmail.com");
+        Email to = new Email(notificationRequest.getRecipient());
+        Content content = new Content("text/plain", notificationRequest.getMailBody());
+        Mail mail = new Mail(from, notificationRequest.getSubject(), to, content);
+
+        SendGrid sg = new SendGrid(apiKey);
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            request.addHeader("", "");
+            Response response = sg.api(request);
+            log.info("response:: {}, {}, {}", response.getStatusCode(), response.getHeaders(), response.getBody());
+            EmailNotificationResponse emailNotificationResponse = new EmailNotificationResponse();
+            if (response.getStatusCode() >= 200 && response.getStatusCode() < 300)
+                emailNotificationResponse.setStatus(true);
+            else emailNotificationResponse.setStatus(false);
+            return emailNotificationResponse;
+        } catch (IOException ex) {
+            throw ex;
+        }
+    }
+}
