5d30cbb279e4d8ebdbc61b77fc493ebb415a9eeb
yusuf usman <yusuf.usman1003@gmail.com>
2025-09-01 16:51:40 +0100

    update

--- src/main/java/dev/litebank/security/config/SecurityConfig.java
+++ src/main/java/dev/litebank/security/config/SecurityConfig.java
@@ -1,8 +1,12 @@
-package dev.litebank.security;
+package dev.litebank.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,5 +33,8 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+
+
 
 }
