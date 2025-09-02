4862f6ddb6da48a3ec7884c0ffe0c04406b80103
yusuf usman <yusuf.usman1003@gmail.com>
2025-09-02 12:01:15 +0100

    update

---
+++ src/main/java/dev/litebank/security/model/User.java
@@ -0,0 +1,32 @@
+package dev.litebank.security.model;
+
+import dev.litebank.dto.responses.AccountResponse;
+import dev.litebank.model.Account;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    private AccountResponse accountResponse;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        //TODO: Remove hardcoded authority
+        return List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return accountResponse.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return accountResponse.getUsername();
+    }
+}
