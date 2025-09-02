910c0b6ab45d3a105b83c4bead37c1a6016bca1b
yusuf usman <yusuf.usman1003@gmail.com>
2025-09-02 12:00:18 +0100

    update

--- src/main/java/dev/litebank/security/config/SecurityConfig.java
+++ src/main/java/dev/litebank/security/config/SecurityConfig.java
@@ -1,40 +1,82 @@
 package dev.litebank.security.config;
 
+import dev.litebank.security.config.filter.LiteBankAuthenticationFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.sql.DataSource;
 
 @Configuration
+@AllArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    private final LiteBankAuthenticationFilter authenticationFilter;
 
-    }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/account/create").permitAll()
-                        .requestMatchers("/api/v1/account/deposit").permitAll()
-                        .requestMatchers("/api/v1/transaction/**").permitAll()
-                        .anyRequest().authenticated()
-                );
-
-        return http.build();
+        return http
+                .csrf(c ->c.disable())
+                .cors(Customizer.withDefaults())
+                .addFilterAt(authenticationFilter, BasicAuthenticationFilter.class)
+                .authorizeHttpRequests(c->c.requestMatchers(HttpMethod.POST,"/login",
+                        "/api/v1/account/create").permitAll())
+                .authorizeHttpRequests(c->c.anyRequest().authenticated())
+                .build();
     }
 
 
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//
+//    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(@Autowired DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
+
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/api/v1/account/create").permitAll()
+//                        .requestMatchers("/api/v1/account/deposit").permitAll()
+//                        .requestMatchers("/api/v1/transaction/**").permitAll()
+//                        .anyRequest().authenticated()
+//                );
+//
+//        return http.build();
+//    }
+
+
+
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+//
+//    }
 
 
 }
