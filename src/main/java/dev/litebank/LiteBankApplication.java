595d782c1a54270af97dc9f8ec105a11b22e8fd6
yusuf usman <yusuf.usman1003@gmail.com>
2025-09-02 11:59:10 +0100

    update

--- src/main/java/dev/litebank/LiteBankApplication.java
+++ src/main/java/dev/litebank/LiteBankApplication.java
@@ -1,7 +1,15 @@
 package dev.litebank;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class LiteBankApplication {
@@ -9,6 +17,14 @@ public class LiteBankApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LiteBankApplication.class, args);
 	}
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+
+    }
 
 
 }
