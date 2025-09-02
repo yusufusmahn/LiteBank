ce80289b699a40733975bfea5b31e8861d6f6a0b
yusuf usman <yusuf.usman1003@gmail.com>
2025-09-02 12:00:54 +0100

    update

---
+++ src/main/java/dev/litebank/security/service/LiteBankUserDetailsService.java
@@ -0,0 +1,23 @@
+package dev.litebank.security.service;
+
+import dev.litebank.dto.responses.AccountResponse;
+import dev.litebank.security.model.User;
+import dev.litebank.service.AccountService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class LiteBankUserDetailsService implements UserDetailsService {
+
+    private final AccountService  accountService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AccountResponse accountResponse = accountService.getByUsername(username);
+        return new User(accountResponse);
+    }
+}
