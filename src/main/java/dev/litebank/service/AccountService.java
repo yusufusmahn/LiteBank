d028e7242067bb24ed6530b972727fc0986a59bd
yusuf usman <yusuf.usman1003@gmail.com>
2025-09-02 11:58:55 +0100

    update

--- src/main/java/dev/litebank/service/AccountService.java
+++ src/main/java/dev/litebank/service/AccountService.java
@@ -2,6 +2,7 @@ package dev.litebank.service;
 
 import dev.litebank.dto.requests.DepositRequest;
 import dev.litebank.dto.requests.CreateAccountRequest;
+import dev.litebank.dto.responses.AccountResponse;
 import dev.litebank.dto.responses.CreateAccountResponse;
 import dev.litebank.dto.responses.DepositResponse;
 import dev.litebank.dto.responses.ViewAccountResponse;
@@ -15,4 +16,6 @@ public interface AccountService {
     ViewAccountResponse viewDetailsFor(String number);
 
     CreateAccountResponse createAccount(CreateAccountRequest createAccountRequest);
+
+    AccountResponse getByUsername(String username);
 }
