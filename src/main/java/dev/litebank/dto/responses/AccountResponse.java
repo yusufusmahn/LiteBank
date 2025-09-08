04185f112b20184798d23459d3a6ad8a50f9c44b
yusuf usman <yusuf.usman1003@gmail.com>
2025-09-08 16:11:21 +0100

    update

--- src/main/java/dev/litebank/dto/responses/AccountResponse.java
+++ src/main/java/dev/litebank/dto/responses/AccountResponse.java
@@ -1,7 +1,10 @@
 package dev.litebank.dto.responses;
 
+import dev.litebank.model.Authority;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Set;
 
 @Getter
 @Setter
@@ -9,4 +12,5 @@ public class AccountResponse {
     private String username;
     private String password;
     private  String accountNumber;
+    private Set<Authority> authorities;
 }
