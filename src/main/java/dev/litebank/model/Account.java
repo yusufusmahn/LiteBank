f23712b992b8532e082f4e381fe45c182fc16c4a
yusuf usman <yusuf.usman1003@gmail.com>
2025-09-08 16:10:55 +0100

    update

--- src/main/java/dev/litebank/model/Account.java
+++ src/main/java/dev/litebank/model/Account.java
@@ -5,6 +5,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Getter
 @Setter
@@ -12,12 +15,21 @@ public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    @Column(unique = true, nullable = false)
     private String username;
     private String password;
     private String name;
     private String accountNumber;
+    private Set<Authority> authorities;
 
     @Enumerated(EnumType.STRING)
     private AccountType accountType;
+
+
+    public Account(){
+        this.authorities = new HashSet<>();
+        authorities.add(Authority.ACCOUNT);
+    }
 
 }
