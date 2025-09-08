fa92bb3e1909beac7392a3acc9ff85ff1b1400ae
yusuf usman <yusuf.usman1003@gmail.com>
2025-09-08 16:11:54 +0100

    update

--- src/main/java/dev/litebank/util/ProjectUtil.java
+++ src/main/java/dev/litebank/util/ProjectUtil.java
@@ -3,4 +3,8 @@ package dev.litebank.util;
 public class ProjectUtil {
     public final static int DEFAULT_PAGE_SIZE = 10;
     public final static int DEFAULT_PAGE_NUMBER = 1;
+    public final static String[] PUBLIC_ENDPOINTS = {
+            "/login",
+            "/api/v1/account/create"
+    };
 }
