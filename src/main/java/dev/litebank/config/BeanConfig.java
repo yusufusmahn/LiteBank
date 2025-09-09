fcfc07d1144c1c8e4df75057cc0f12a177ee36eb
yusuf usman <yusuf.usman1003@gmail.com>
2025-09-09 11:16:54 +0100

    update

--- src/main/java/dev/litebank/config/BeanConfig.java
+++ src/main/java/dev/litebank/config/BeanConfig.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BeanConfig {
@@ -18,5 +20,11 @@ public class BeanConfig {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+
     }
 }
