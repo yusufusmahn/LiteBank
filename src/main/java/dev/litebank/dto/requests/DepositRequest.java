5e2d18a140fd131c25a271f8ff2213b0e109f54e
yusuf usman <yusuf.usman1003@gmail.com>
2025-09-09 11:17:29 +0100

    update

--- src/main/java/dev/litebank/dto/requests/DepositRequest.java
+++ src/main/java/dev/litebank/dto/requests/DepositRequest.java
@@ -1,6 +1,9 @@
 package dev.litebank.dto.requests;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.litebank.model.PaymentMethod;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,8 +13,11 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 public class DepositRequest {
-
+    @Size(min = 10, max = 10, message = "account number must be 10 digits long")
     private String accountNumber;
+    @Min(value = 50, message = "minimum deposit amount = 50")
     private BigDecimal amount;
-    private PaymentMethod PaymentMethod;
+
+    @JsonProperty("payment_method")
+    private PaymentMethod paymentMethod;
 }
